66bebeedf36eac4bb18166530692747745d5fa7c
Eriniss <jhs2195@gmail.com>
2025-03-03 17:19:38 +0900

    :beetle: id값 오류로 인한 단일 User 생성 기능 버그 수정

---
+++ models/post.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// Reply (댓글) 모델
+type Reply struct {
+	gorm.Model
+
+	PostID   uint   `json:"post_id"` // Post와 관계를 형성하는 외래 키
+	Email    string `json:"email"`
+	Contents string `json:"contents"`
+}
+
+// Post (게시글) 모델
+type Post struct {
+	gorm.Model
+
+	Email    string   `json:"email"`
+	Title    string   `json:"title"`
+	Contents string   `json:"contents"`
+	Tags     []string `gorm:"serializer:json" json:"tags"`
+	Replies  []Reply  `gorm:"foreignKey:PostID" json:"replies"`
+	Private  bool     `json:"private"`
+}
--- models/user.go
+++ models/user.go
@@ -1,18 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
 
-	Email     string     `gorm:"unique" json:"email"`
-	Password  string     `json:"-"` // Password는 json 객체 반환에서 제외
-	Role      int        `json:"role"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	Email    string `gorm:"unique" json:"email"`
+	Password string `json:"-"` // Password는 json 객체 반환에서
+	Role     uint   `json:"role"`
 }
